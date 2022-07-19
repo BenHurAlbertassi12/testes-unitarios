bf5e63a1f88fbc6d9604aa99b2faaea6ca763fe7
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-19 18:40:03 -0300

    Desafio 1 Average concluido

--- src/average.js
+++ src/average.js
@@ -11,7 +11,24 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-// adicione seu código aqui
+const average = (arrayio) => {
+  // variaveis
+  let somaArrey = 0;
+  let divArrey = 0;
+  let resultado = 0;
 
+  // comando if para quando o tamanho do array for igual a 0
+  if (arrayio.length === 0) {
+    return undefined;
+  } // for para percorrer a array
+  for (let index = 0; index < arrayio.length; index += 1) {
+    if (typeof arrayio[index] !== 'number') { // if para enviar mensagem de undefined quando algo digitado não fo number
+      return undefined; // retorno pelo if
+    }
+    somaArrey += arrayio[index]; // soma vinda do for ali de cima, que percorreu toda array
+    divArrey = arrayio.length; // array .length de novo para chegar ao numero que sera o divisor
+    resultado = Math.round(somaArrey / divArrey); // resultado arredondado com Math.round 
+  }
+  return resultado;
+};
 module.exports = average;
