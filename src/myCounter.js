59e790786ca76e4e6c623a411d0148f3c0aa5173
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-20 16:28:09 -0300

    Requisito 6

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,10 +16,10 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var newCounter2 = 2; newCounter2 <= 3; newCounter2 += 1) {
+    for (let newCounter2 = 2; newCounter2 <= 3; newCounter2 += 1) {
       myArray.push(newCounter2);
     }
   }
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,18 +30,31 @@ const productDetails = require("../src/productDetails");
 */
 
 describe("6 - Implemente os casos de teste para a função `productDetails`", () => {
-  //it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-  fail('Teste vazio!');
+  // it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+  // fail('Teste vazio!');
   // ESCREVA SEUS TESTES ABAIXO:
   it(" Teste se productDetails é uma função.", () => {});
+  expect(typeof productDetails).toBe('function');
 
-  it(" Teste se o retorno da função é um array.", () => {});
+  it(" Teste se o retorno da função é um array.", () => {
+    expect(productDetails()).toBeInstanceOf(Array);
+  });
 
-  it("Teste se o array retornado pela função contém dois itens dentro.", () => {});
+  it("Teste se o array retornado pela função contém dois itens dentro.", () => {
+    expect(productDetails('Alcool gel', 'Máscara').length).toEqual(2);
+  });
 
-  it("Teste se os dois itens dentro do array retornado pela função são objetos.", () => {});
+  it("Teste se os dois itens dentro do array retornado pela função são objetos.", () => {
+    expect(productDetails('Alcool gel', 'Máscara')).toBeInstanceOf(Object);
+  });
 
-  it(" Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.", () => {});
+  it(" Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.", () => {
+    expect(productDetails('Alcool gel', 'Máscara')[0] !== (productDetails('Alcool gel', 'Máscara')[1])).toBe(true);
+  });
 
-  it("Teste se os dois productIds terminam com 123.", () => {});
+  it("Teste se os dois productIds terminam com 123.", () => {
+    expect(productDetails('Alcool gel', 'Máscara')[0,1].details.productId).toContain('123');
+  });
 });
+
+//consegui realizar o exercicio atravez da documentação => https://jestjs.io/docs/expect
